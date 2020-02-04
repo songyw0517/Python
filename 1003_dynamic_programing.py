0a7b3737f24c43683f5c90d870a6d618f8f6ce4b
songyw0517 <songyw0517@naver.com>
2020-02-04 16:48:44 +0900

    문제3 해결!

--- 1003_dynamic_programing.py
+++ 1003_dynamic_programing.py
@@ -1,26 +1,16 @@
-global cach
-cach = [0 for i in range(41)]
-def fibonacci(n):
-    if n<=1:
-        return cach[n]
-    
-    elif cach[n]!=0:
-        return cach[n]
-
-    else:
-        return cach[n] = int(int(fibonacci(n-1)) + int(fibonacci(n-2)))
-
-cach[1]=1
-print(cach)
-print(type(cach))
-print(type(fibonacci(2)))
 test=int(input(''))
+item=[]
+fibo=[1,1]
 for i in range(test):
-    num = int(input(''))
-    if num==0:
-        print("1 0")
-    elif num==1:
-        print("0, 1")
+    item.append(int(input('')))
+    
+for i in range(2, max(item)):
+    fibo.append(fibo[i-1]+fibo[i-2])
+while item != []:
+    value = item.pop(0)
+    if value==0:
+        print('1 0')
+    elif value==1:
+        print('0 1')
     else:
-        fibonacci(num)
-        #print(cach[num-1]. cach[num])
+        print(fibo[value-2], fibo[value-1])
